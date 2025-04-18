9d90870660b8eb09d089659a7c70daf9f8f2a1a5
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-04-18 14:15:04 +0530

    stored user data on local storage to

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule, AbstractControl, ValidatorFn } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @Component({
@@ -12,43 +12,58 @@ export class RegistrationComponent implements OnInit{
     registerForm!: FormGroup;
     submitted = false;
     showpassword: boolean = false;
+    //for local store 
+    user :any = {}
      
     constructor(private formBuilder: FormBuilder) {}
-  
-    ngOnInit() {
-      this.registerForm = this.formBuilder.group({
-        firstName: ['', Validators.required,Validators.pattern('^[A-Z][a-zA-Z]{2,}$')],
-        lastName: ['', Validators.required, Validators.pattern('^[A-Z][a-zA-Z]{2,}$')],
-        dob: this.formBuilder.group({
-          start: ['', Validators.required],
-          end: ['', Validators.required]
-        }),
-        email: ['', [Validators.required, Validators.email]],
-        gender:['',[Validators.required]],
-        password: ['', [Validators.required, Validators.minLength(6)]],
-        confirmPassword: ['', Validators.required],
-      });
-    }
-    onsubmit() {
-      if (this.registerForm.valid) {
-         console.log('onsubmit function called', this.registerForm.value);
-        let data = {
-          firstName: this.registerForm.value.firstName,
-          lastName: this.registerForm.value.lastName,
-          dob:this.registerForm.value.dob,
-          email: this.registerForm.value.email,
-          password: this.registerForm.value.password,
-          confirmPassword: this.registerForm.value.confirmPassword,
 
-        };
+    ngOnInit(){
+      this.createRegistrationForm();
         
+    }
+    createRegistrationForm(){
+      this.registerForm = this.formBuilder.group({
+            firstName: ['',[ Validators.required,Validators.pattern('^[A-Z][a-zA-Z]{2,}$')]],
+            lastName: ['', [Validators.required, Validators.pattern('^[A-Z][a-zA-Z]{2,}$')]],
+            dob:['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            gender:['',[Validators.required]],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            confirmPassword: ['', Validators.required],
+          },{
+            validators: this.matchPassword('password', 'confirmPassword')  
+          });
+    }
+
+    onsubmit(){
+      this.submitted = true;
+      if (this.registerForm.valid){
+      console.log(this.registerForm.value);
+      this.user = this.registerForm.value
+      localStorage.setItem('Users',JSON.stringify(this.user));
       }
+    }
+  
+    matchPassword(password: string, confirmPassword: string): ValidatorFn {
+      return (formGroup: AbstractControl): { [key: string]: any } | null => {
+        const pass = formGroup.get(password);
+        const confirmPass = formGroup.get(confirmPassword);
     
-      
+        if (pass && confirmPass && pass.value !== confirmPass.value) {
+          confirmPass.setErrors({ passwordMismatch: true });
+          return { passwordMismatch: true };
+        } else {
+          if (confirmPass?.hasError('passwordMismatch')) {
+            confirmPass.setErrors(null);
+          }
+          return null;
+        }
+      }
     }
     showhidepassword(){
       this.showpassword = !this.showpassword;
     }
   }
-  
 
+  
+    
